1220447c78c45c74eb449d76f283cce4a438a1bd
LLMA-dot <66136128+LLMA-dot@users.noreply.github.com>
2023-01-07 16:41:30 +0100

    Update Chapter 06 - The Many Forms of Scripting.ps1

--- Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter 06 - The Many Forms of Scripting.ps1
+++ Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter 06 - The Many Forms of Scripting.ps1
@@ -5,8 +5,26 @@
 
 # This chapter deals with the concepts of Tools vs. Constroller Scripts - super-interesting stuff here!
 
-# Example 1
+# Example 1 - not actual code
 Get-CustomerRecord |
 Where-Object {$_LastActivity -lt (Get-Date).AddDays(-365)} |
 ConvertTo-HTML |
 Out-File \\intranet\www\reports\inactive-customers.html
+
+# Example 2 - not actual code
+Get-EnabledNonExpiringUser | 
+Add-ExpiryDataToUser |
+Where-Object {$_.DaystoExpire -lt 2} |
+Send-PasswordExpiryMessageToUser |
+Export-CSV report.csv
+
+# Example 3 - not actual code
+# Get-ComplianceInfo.ps1
+Get-ADComputer -filter * | foreach {
+	if (Test-Connection $_.name -quiet) {
+		msinfo32 /computer "\\$($_.name)" /report "C:\work\$($_.name)-msinfo.txt"
+	}
+	else {
+		$_.name | out-file C:\logs\offline.txt -Append
+	}
+}
