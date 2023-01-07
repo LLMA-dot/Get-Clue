faccba042b7c770abd91acd80c95c49b9f726f60
LLMA-dot <66136128+LLMA-dot@users.noreply.github.com>
2023-01-07 16:36:05 +0100

    Update Chapter 06 - The Many Forms of Scripting.ps1

--- Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter 06 - The Many Forms of Scripting.ps1
+++ Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter 06 - The Many Forms of Scripting.ps1
@@ -1,1 +1,12 @@
+# PowerShell Scripting in a Month of Lunches
+# by Don Jones & Jeff Hicks
 
+# Chapter 6: The Many Forms of Scripting (and which to use)
+
+# This chapter deals with the concepts of Tools vs. Constroller Scripts - super-interesting stuff here!
+
+# Example 1
+Get-CustomerRecord |
+Where-Object {$_LastActivity -lt (Get-Date).AddDays(-365)} |
+ConvertTo-HTML |
+Out-File \\intranet\www\reports\inactive-customers.html
