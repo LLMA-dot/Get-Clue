7b3405d971d9146104460167658d17c502ce5c8b
LLMA-dot <66136128+LLMA-dot@users.noreply.github.com>
2023-01-29 20:29:29 +0100

    Update Chapter_10_BasicFunction.ps1

--- Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter_10_BasicFunction.ps1
+++ Languages/PowerShell/Books/PowerShell Scripting In A Month of Lunches/Chapter_10_BasicFunction.ps1
@@ -1,3 +1,4 @@
+# First iteration
 function Get-MachineInfo {
 	Param(
 		[string[]]$ComputerName,
@@ -26,6 +27,39 @@ function Get-MachineInfo {
 			
 			# Output data
 			# TODO
+		
+		} #foreach
+} #function
+
+# With data
+function Get-MachineInfo {
+	Param(
+		[string[]]$ComputerName,
+		[string]$LogFailuresToPath,
+		[string]$Protocol = "Wsman",
+		[switch]$ProtocolFallback
+		)
+		
+		foreach ($computer in $computername){
+		
+			#Establish Session Protocol
+			if ($protocol -eq 'Dcom') {
+				$option = New-CimSessionOption -Protocol Dcom
+			} else {
+				$option = New-CimSessionOption -Protocol Wsman
+			}
+		
+			# Connect Session
+			$session = New-CimSession -ComputerName $computer -SessionOption $option
+			
+			# Query data
+			$os = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $session
+			
+			# Close session
+			$session | Remove-CimSession
+			
+			# Output data
+			$os | Select-Object -Property @{n='ComputerName';e={$computer}}, Version, ServicePackMajorVersion
 		
 		} #foreach
 } #function
